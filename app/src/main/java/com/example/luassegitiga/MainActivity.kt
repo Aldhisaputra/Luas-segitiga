773ceb2fff8fa8a8c5d58461514c73bf3d461d9e
Aldhi Saputra <aldhisaputra795@gmail.com>
2020-01-04 20:47:11 +0700

    LUASSEGITIGA

--- app/src/main/java/com/example/luassegitiga/MainActivity.kt
+++ app/src/main/java/com/example/luassegitiga/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var edt_LongPedestal: EditText? = null
+    var edt_Pedestal: EditText? = null
     var edt_Height: EditText? = null
     var btn_Hitung: Button? = null
     var edt_Result: EditText? = null
@@ -17,25 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edt_LongPedestal = findViewById(R.id.edt_LongPedestal)
+        edt_Pedestal = findViewById(R.id.edt_Pedestal)
         edt_Height = findViewById(R.id.edt_Height)
         btn_Hitung = findViewById(R.id.btn_Hitung)
         edt_Result = findViewById(R.id.edt_Result)
 
         btn_Hitung?.setOnClickListener {
-            var Long = edt_LongPedestal?.text.toString()
+            var Pedestal = edt_Pedestal?.text.toString()
             var Height = edt_Height?.text.toString()
-            var new =
 
 
-        if (Long == "")   {
+
+        if (Pedestal == "")   {
             Toast.makeText(this@MainActivity, "Panjang Alas  harus di isi", Toast.LENGTH_SHORT).show()
 
 
         }else if (Height == "")  {
             Toast.makeText(this@MainActivity, "Tinggi harus di isi", Toast.LENGTH_SHORT).show()
         }else{
-            var large = Long.toInt()  * Height.toInt()
+            var large = 0.5 * Pedestal.toInt()  * Height.toInt()
             edt_Result?.setText("$large")
         }
 
